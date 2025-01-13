317c8f3077e565612162c6458a6dec003176c877
loveyMelon <manciepmc@gmail.com>
2025-01-13 19:24:14 +0900

    Initial Commit

---
+++ Patata/PatataApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatataApp.swift
+//  Patata
+//
+//  Created by 김진수 on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PatataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
