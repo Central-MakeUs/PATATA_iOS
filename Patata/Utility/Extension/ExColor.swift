f71c6fa251393f11b43637ee2fb9f1e105acfb48
loveyMelon <manciepmc@gmail.com>
2025-01-14 21:33:54 +0900

    refactor: 색상 시스템 구조 개선

--- Patata/Utility/Extension/ExColor.swift
+++ Patata/Utility/Extension/ExColor.swift
@@ -6,23 +6,63 @@
 //
 
 import SwiftUI
+ 
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
 
-extension UIColor {
-    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if hexFormatted.hasPrefix("#") {
-            hexFormatted = String(hexFormatted.dropFirst())
-        }
-        
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-        
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-        
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
+extension Color {
+   // Blue colors
+   static let blue10 = Color(hex: "E6F7FF")
+   static let blue20 = Color(hex: "BAE7FF")
+   static let blue50 = Color(hex: "91D5FF")
+   static let blue100 = Color(hex: "69C0FF")
+   
+   // Navy colors
+   static let navy20 = Color(hex: "405493")
+   static let navy50 = Color(hex: "172A5D")
+   static let navy100 = Color(hex: "020514")
+   
+   // Yellow colors
+   static let yellow20 = Color(hex: "FDF2C4")
+   static let yellow100 = Color(hex: "FFDD50")
+   
+   // Red colors
+   static let red20 = Color(hex: "FBCCCB")
+   static let red100 = Color(hex: "EF4743")
+   
+   // Gray colors
+   static let gray10 = Color(hex: "F6F7F8")
+   static let gray20 = Color(hex: "EBEFF3")
+   static let gray30 = Color(hex: "D9DEE5")
+   static let gray40 = Color(hex: "CACFD8")
+   static let gray50 = Color(hex: "B6BDC7")
+   static let gray60 = Color(hex: "A0A7B1")
+   static let gray70 = Color(hex: "808791")
+   static let gray80 = Color(hex: "555A63")
+   static let gray90 = Color(hex: "2E3035")
+   static let gray100 = Color(hex: "1C1C1C")
+   
+   // Semantic colors
+   // Text colors
+   static let textDefault = Color(hex: "1C1C1C")    // 기본 텍스트, Heading에 사용
+   static let textSub = Color(hex: "2E3035")        // 두번째 텍스트, 본문에 사용
+   static let textInfo = Color(hex: "727883")       // 부가정보 텍스트, 유의사항 등
+   static let textDisabled = Color(hex: "B0B5BF")   // 비활성화된 텍스트
+   
+   // Background & Border colors
+   static let border = Color(hex: "CACED8")         // 영역을 구분할 때 사용하는 구분선
+   static let bgGrouped = Color(hex: "F6F7F8")      // 페이지 내에서 특정 컨텐츠 영역 구분시 사용
+   static let bgOverlay = Color(hex: "1C1C1C").opacity(0.3)  // 팝업이 뜰 때 뒷면에 오버레이
 }
---
+++ Patata/Utility/Extension/ExShapeStyle.swift
@@ -0,0 +1,53 @@
+//
+//  ExShapeStyle.swift
+//  Patata
+//
+//  Created by 김진수 on 1/14/25.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+   // Blue colors
+   static var blue10: Color { Color.blue10 }
+   static var blue20: Color { Color.blue20 }
+   static var blue50: Color { Color.blue50 }
+   static var blue100: Color { Color.blue100 }
+   
+   // Navy colors
+   static var navy20: Color { Color.navy20 }
+   static var navy50: Color { Color.navy50 }
+   static var navy100: Color { Color.navy100 }
+   
+   // Yellow colors
+   static var yellow20: Color { Color.yellow20 }
+   static var yellow100: Color { Color.yellow100 }
+   
+   // Red colors
+   static var red20: Color { Color.red20 }
+   static var red100: Color { Color.red100 }
+   
+   // Gray colors
+   static var gray10: Color { Color.gray10 }
+   static var gray20: Color { Color.gray20 }
+   static var gray30: Color { Color.gray30 }
+   static var gray40: Color { Color.gray40 }
+   static var gray50: Color { Color.gray50 }
+   static var gray60: Color { Color.gray60 }
+   static var gray70: Color { Color.gray70 }
+   static var gray80: Color { Color.gray80 }
+   static var gray90: Color { Color.gray90 }
+   static var gray100: Color { Color.gray100 }
+   
+   // Semantic colors
+   // Text colors
+   static var textDefault: Color { Color.textDefault }
+   static var textSub: Color { Color.textSub }
+   static var textInfo: Color { Color.textInfo }
+   static var textDisabled: Color { Color.textDisabled }
+   
+   // Background & Border colors
+   static var border: Color { Color.border }
+   static var bgGrouped: Color { Color.bgGrouped }
+   static var bgOverlay: Color { Color.bgOverlay }
+}
