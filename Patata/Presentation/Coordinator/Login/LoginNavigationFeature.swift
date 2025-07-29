fe98009f0b5dc4e473b6caa103a95b1eea5273d8
loveyMelon <manciepmc@gmail.com>
2025-07-29 22:20:24 +0900

    feat: LoginNavigationFeature 구현

---
+++ Patata/Presentation/Coordinator/Login/LoginNavigationFeature.swift
@@ -0,0 +1,64 @@
+//
+//  LoginNavigationFeature.swift
+//  Patata
+//
+//  Created by 김진수 on 7/29/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer(state: .equatable)
+enum LoginPath {
+    case login(LoginFeature)
+    case profileEdit(ProfileEditFeature)
+}
+
+@Reducer
+struct LoginNavigationFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        var routes: StackState<LoginPath.State> = .init()
+        var root = LoginFeature.State()
+    }
+
+    enum Action {
+        case routes(StackActionOf<LoginPath>)
+        case root(LoginFeature.Action)
+        case delegate(Delegate)
+
+        enum Delegate {
+            case loginCompleted
+            case tappedBackButton
+        }
+    }
+
+    var body: some ReducerOf<Self> {
+        Scope(state: \.root, action: \.root) {
+            LoginFeature()
+        }
+        
+        Reduce { state, action in
+            switch action {
+            case .root(.delegate(.loginSuccess)):
+                if UserDefaultsManager.nickname.isEmpty {
+                    state.routes.append(.profileEdit(ProfileEditFeature.State(viewState: .first, profileData: .init())))
+                } else {
+                    return .send(.delegate(.loginCompleted))
+                }
+
+            case .routes(.element(id: _, action: .profileEdit(.delegate(.successChangeNickname)))):
+                return .send(.delegate(.loginCompleted))
+                
+            case let .routes(.element(id: id, action: .profileEdit(.delegate(.tappedBackButton(viewState))))):
+                state.routes.pop(to: id)
+
+            default:
+                break
+            }
+            return .none
+        }
+        .forEach(\.routes, action: \.routes)
+    }
+}
