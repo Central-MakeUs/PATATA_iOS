833a4886b1ceecfa19d7b8747a490d7bc31b6072
loveyMelon <manciepmc@gmail.com>
2025-07-29 22:21:30 +0900

    feat: 로그인 네비게이션 대응

---
+++ Patata/Presentation/Coordinator/Login/LoginNavgationView.swift
@@ -0,0 +1,28 @@
+//
+//  LoginNavgationView.swift
+//  Patata
+//
+//  Created by 김진수 on 7/29/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct LoginNavigationView: View {
+    @Perception.Bindable var store: StoreOf<LoginNavigationFeature>
+
+    var body: some View {
+        WithPerceptionTracking {
+            NavigationStack(path: $store.scope(state: \.routes, action: \.routes)) {
+                LoginView(store: store.scope(state: \.root, action: \.root))
+            } destination: {
+                switch $0.case {
+                case let .profileEdit(store):
+                    ProfileEditView(store: store)
+                default:
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
