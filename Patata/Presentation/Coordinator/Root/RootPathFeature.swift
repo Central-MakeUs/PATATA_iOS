2de52b667eac2d14cafb3381d1c080d1db0589dd
loveyMelon <manciepmc@gmail.com>
2025-07-29 22:22:38 +0900

    feat: Root Path 분리 feature 대응

---
+++ Patata/Presentation/Coordinator/Root/RootPathFeature.swift
@@ -0,0 +1,71 @@
+//
+//  RootPathFeature.swift
+//  Patata
+//
+//  Created by 김진수 on 7/29/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct RootPathFeature {
+    @ObservableState
+    enum State {
+        case splash(SplashFeature.State = .init())
+        case onboarding(OnboardingFeature.State)
+        case login(LoginNavigationFeature.State)
+        case tabBar(TabCoordinator.State)
+        
+        init() { self = .splash() }
+    }
+    
+    @CasePathable
+    enum Action {
+        case _sceneChange(State)
+        
+        case splash(SplashFeature.Action)
+        case onboarding(OnboardingFeature.Action)
+        case login(LoginNavigationFeature.Action)
+        case tabBar(TabCoordinator.Action)
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case let .splash(.delegate(.isFirstUser(isFirst))):
+                if isFirst {
+                    return .send(._sceneChange(.onboarding(.init())))
+                } else {
+                    return .send(._sceneChange(.login(.init())))
+                }
+                
+            case .onboarding(.delegate(.startButtonTapped)):
+                return .send(._sceneChange(.login(LoginNavigationFeature.State())))
+                
+            case .login(.delegate(.loginCompleted)):
+                return .send(._sceneChange(.tabBar(TabCoordinator.State())))
+                
+            case let ._sceneChange(new):
+                state = new
+                
+            default:
+                break
+            }
+            return .none
+        }
+        .ifCaseLet(\.splash, action: \.splash) {
+            SplashFeature()
+        }
+        .ifCaseLet(\.onboarding, action: \.onboarding) {
+            OnboardingFeature()
+        }
+        .ifCaseLet(\.login, action: \.login) {
+            LoginNavigationFeature()
+        }
+        .ifCaseLet(\.tabBar, action: \.tabBar) {
+            TabCoordinator()
+        }
+    }
+}
+
