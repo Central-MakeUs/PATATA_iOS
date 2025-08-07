3be352298776ead6ea41adb8cff0fd7b5510526b
loveyMelon <manciepmc@gmail.com>
2025-08-07 16:28:02 +0900

    feat: PopGesture 기능 구현

---
+++ Patata/Utility/Extension/ExUINavigationController.swift
@@ -0,0 +1,23 @@
+//
+//  ExUINavigationController.swift
+//  Patata
+//
+//  Created by 김진수 on 8/5/25.
+//
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return gestureRecognizer.isEqual(self.interactivePopGestureRecognizer)
+    }
+}
