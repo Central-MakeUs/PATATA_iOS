520ccfa280f5f914f172fb9d955a5102f8b5b011
loveyMelon <manciepmc@gmail.com>
2025-08-11 14:40:05 +0900

    Refactor: ArchiveCoordinator 액션 처리 로직 분리

--- Patata/Presentation/Coordinator/Archive/ArchiveCoordinator.swift
+++ Patata/Presentation/Coordinator/Archive/ArchiveCoordinator.swift
@@ -47,6 +47,7 @@ struct ArchiveCoordinator {
         Scope(state: \.root, action: \.root) {
             ArchiveFeature()
         }
+        
         core()
     }
 }
@@ -57,114 +58,23 @@ extension ArchiveCoordinator {
             state,
             action in
             switch action {
-            case let .root(.delegate(.tappedSpot(spotId))):
-                state.$isHidden.withLock { $0 = true }
-                state.routes.append(.spotDetail(SpotDetailFeature.State(viewState: .other, spotId: spotId)))
-                state.screenIds[.spotDetail] = state.routes.ids.last
+            case let .root(.delegate(action)):
+                return rootAction(state: &state, action: action)
                 
-            case .root(.delegate(.tappedConfirmButton)):
-                state.$isHidden.withLock { $0 = true }
-                state.routes.append(.category(SpotCategoryFeature.State(initialIndex: 0)))
-                state.screenIds[.category] = state.routes.ids.last
-                
-            case let .router(.element(id: _, action: .spotDetail(.delegate(.report(type, id))))):
-                if type == "Post" {
-                    state.routes.append(.report(ReportFeature.State(viewState: .post, id: id)))
-                } else {
-                    state.routes.append(.report(ReportFeature.State(viewState: .user, id: id)))
+            case let .router(action):
+                switch action {
+                case let .element(id: _, action: action):
+                    return routerAction(state: &state, action: action)
+                    
+                default:
+                    break
                 }
-                state.screenIds[.report] = state.routes.ids.last
-                
-            case let .router(.element(id: _, action: .spotDetail(.delegate(.reviewReport(id: id))))):
-                state.routes.append(.report(ReportFeature.State(viewState: .review, id: id)))
-                state.screenIds[.report] = state.routes.ids.last
-                
-            case .router(.element(id: _, action: .spotDetail(.delegate(.tappedNavBackButton(_, _))))):
-                state.$isHidden.withLock { $0 = false }
-                _ = state.routes.popLast()
-                
-            case .router(.element(id: _, action: .spotDetail(.delegate(.delete(_))))):
-                state.$isHidden.withLock { $0 = false }
-                _ = state.routes.popLast()
-                state.errorMSG = "게시물이 정상적으로 삭제되었습니다."
-                state.popupIsPresent = true
-                
-            case let .router(.element(id: _, action: .spotDetail(.delegate(.deleteSpot(msg, _))))):
-                _ = state.routes.popLast()
-                state.errorMSG = msg
-                state.popupIsPresent = true
-                
-            case let .router(.element(id: _, action: .spotDetail(.delegate(.editSpotDetail(spotDetail, _))))):
-                state.routes.append(
-                    .spotedit(
-                        SpotEditorFeature.State(
-                            viewState: .edit,
-                            spotDetail: spotDetail,
-                            spotLocation: spotDetail.spotCoord,
-                            spotAddress: spotDetail.spotAddress,
-                            imageDatas: [],
-                            beforeViewState: .other
-                        )
-                    )
-                )
-                state.screenIds[.spotedit] = state.routes.ids.last
-                
-            case let .router(.element(id: _, action: .addSpotMap(.delegate(.tappedAddConfirmButton(coord, spotAddresss, _, _, _))))):
-                _ = state.routes.popLast()
-                
-                if let id = state.screenIds[.spotedit] {
-                    return .run { send in
-                        await send(.router(.element(id: id, action: .spotedit(.parentsAction(.changeAddress(coord, spotAddresss))))))
-                    }
-                }
-                
-            case .router(.element(id: _, action: .addSpotMap(.delegate(.tappedBackButton(_))))):
-                _ = state.routes.popLast()
-
-            case .router(.element(id: _, action: .spotedit(.delegate(.tappedBackButton)))):
-                _ = state.routes.popLast()
-                
-            case .router(.element(id: _, action: .spotedit(.delegate(.tappedXButton)))):
-                state.routes.removeAll()
-                state.$isHidden.withLock { $0 = false }
-                
-            case .router(.element(id: _, action: .spotedit(.delegate(.successSpotEdit)))):
-                state.errorMSG = "게시물이 수정되었습니다."
-                _ = state.routes.popLast()
-                state.popupIsPresent = true
-                
-            case let .router(.element(id: _, action: .spotedit(.delegate(.tappedLocation(coord, _, spotDetail, _))))):
-                state.routes.append(
-                    .addSpotMap(
-                        AddSpotMapFeature.State(
-                            viewState: .edit,
-                            spotDetailEntity: spotDetail,
-                            datas: [],
-                            spotCoord: coord
-                        )
-                    )
-                )
                 
             case .viewEvent(.dismissPopup):
                 state.popupIsPresent = false
                 
             case .viewEvent(.dismissAlert):
                 state.alertIsPresent = false
-                
-            case .router(.element(id: _, action: .report(.delegate(.tappedBackButton)))):
-                _ = state.routes.popLast()
-                
-            case .router(.element(id: _, action: .report(.delegate(.tappedConfirmButton)))):
-                state.routes.removeAll()
-                state.$isHidden.withLock { $0 = false }
-                state.alertIsPresent = true
-                
-            case .router(.element(id: _, action: .category(.delegate(.tappedNavBackButton)))):
-                state.$isHidden.withLock { $0 = false }
-                _ = state.routes.popLast()
-                
-            case let .router(.element(id: _, action: .category(.delegate(.tappedSpot(spotId))))):
-                state.routes.append(.spotDetail(SpotDetailFeature.State(viewState: .other, spotId: spotId)))
                 
             case let .bindingPopupIsPresent(isPresent):
                 state.popupIsPresent = isPresent
@@ -180,6 +90,189 @@ extension ArchiveCoordinator {
         }
         .forEach(\.routes, action: \.router)
     }
+    
+    private func rootAction(state: inout ArchiveCoordinator.State, action: ArchiveFeature.Action.Delegate) -> Effect<Action> {
+        switch action {
+        case let .tappedSpot(spotId):
+            state.$isHidden.withLock { $0 = true }
+            state.routes.append(.spotDetail(SpotDetailFeature.State(viewState: .other, spotId: spotId)))
+            state.screenIds[.spotDetail] = state.routes.ids.last
+            
+        case .tappedConfirmButton:
+            state.$isHidden.withLock { $0 = true }
+            state.routes.append(.category(SpotCategoryFeature.State(initialIndex: 0)))
+            state.screenIds[.category] = state.routes.ids.last
+        }
+        
+        return .none
+    }
+    
+    private func routerAction(state: inout ArchiveCoordinator.State, action: ArchiveCoorPath.Action) -> Effect<Action> {
+        switch action {
+        case let .spotDetail(.delegate(action)):
+            return spotDetailAction(state: &state, action: action)
+            
+        case let .spotedit(.delegate(action)):
+            return spotEditAction(state: &state, action: action)
+            
+        case let .addSpotMap(.delegate(action)):
+            return addSpotAction(state: &state, action: action)
+            
+        case let .report(.delegate(action)):
+            return reportAction(state: &state, action: action)
+            
+        case let .category(.delegate(action)):
+            return categoryAction(state: &state, action: action)
+            
+        default:
+            break
+        }
+        
+        return .none
+    }
+    
+    private func spotDetailAction(
+        state: inout ArchiveCoordinator.State,
+        action: SpotDetailFeature.Action.Delegate
+    ) -> Effect<Action> {
+        switch action {
+        case .tappedNavBackButton(_, _):
+            state.$isHidden.withLock { $0 = false }
+            _ = state.routes.popLast()
+            
+        case .delete(_):
+            state.$isHidden.withLock { $0 = false }
+            _ = state.routes.popLast()
+            state.errorMSG = "게시물이 정상적으로 삭제되었습니다."
+            state.popupIsPresent = true
+            
+        case let .editSpotDetail(spotDetail, _):
+            state.routes.append(
+                .spotedit(
+                    SpotEditorFeature.State(
+                        viewState: .edit,
+                        spotDetail: spotDetail,
+                        spotLocation: spotDetail.spotCoord,
+                        spotAddress: spotDetail.spotAddress,
+                        imageDatas: [],
+                        beforeViewState: .other
+                    )
+                )
+            )
+            state.screenIds[.spotedit] = state.routes.ids.last
+            
+        case let .report(type, id):
+            if type == "Post" {
+                state.routes.append(.report(ReportFeature.State(viewState: .post, id: id)))
+            } else {
+                state.routes.append(.report(ReportFeature.State(viewState: .user, id: id)))
+            }
+            state.screenIds[.report] = state.routes.ids.last
+            
+        case let .reviewReport(id):
+            state.routes.append(.report(ReportFeature.State(viewState: .review, id: id)))
+            state.screenIds[.report] = state.routes.ids.last
+            
+        case let .deleteSpot(msg, _):
+            _ = state.routes.popLast()
+            state.errorMSG = msg
+            state.popupIsPresent = true
+        }
+        
+        return .none
+    }
+    
+    private func addSpotAction(
+        state: inout ArchiveCoordinator.State,
+        action: AddSpotMapFeature.Action.Delegate
+    ) -> Effect<Action> {
+        switch action {
+        case .tappedBackButton(_):
+            _ = state.routes.popLast()
+            
+        case let .tappedAddConfirmButton(coord, spotAddresss, _, _, _):
+            _ = state.routes.popLast()
+            
+            if let id = state.screenIds[.spotedit] {
+                return .run { send in
+                    await send(.router(.element(id: id, action: .spotedit(.parentsAction(.changeAddress(coord, spotAddresss))))))
+                }
+            }
+        }
+        
+        return .none
+    }
+    
+    private func spotEditAction(
+        state: inout ArchiveCoordinator.State,
+        action: SpotEditorFeature.Action.Delegate
+    ) -> Effect<Action> {
+        switch action {
+        case .tappedBackButton:
+            _ = state.routes.popLast()
+            
+        case .tappedXButton:
+            state.routes.removeAll()
+            state.$isHidden.withLock { $0 = false }
+            
+        case let .tappedLocation(coord, _, spotDetail, _):
+            state.routes.append(
+                .addSpotMap(
+                    AddSpotMapFeature.State(
+                        viewState: .edit,
+                        spotDetailEntity: spotDetail,
+                        datas: [],
+                        spotCoord: coord
+                    )
+                )
+            )
+            
+        case .successSpotEdit(_):
+            state.errorMSG = "게시물이 수정되었습니다."
+            _ = state.routes.popLast()
+            state.popupIsPresent = true
+            
+        default:
+            break
+        }
+        
+        return .none
+    }
+    
+    private func reportAction(
+        state: inout ArchiveCoordinator.State,
+        action: ReportFeature.Action.Delegate
+    ) -> Effect<Action> {
+        switch action {
+        case .tappedConfirmButton:
+            state.routes.removeAll()
+            state.$isHidden.withLock { $0 = false }
+            state.alertIsPresent = true
+            
+        case .tappedBackButton:
+            _ = state.routes.popLast()
+        }
+        
+        return .none
+    }
+    
+    private func categoryAction(
+        state: inout ArchiveCoordinator.State,
+        action: SpotCategoryFeature.Action.Delegate
+    ) -> Effect<Action> {
+        switch action {
+        case .tappedNavBackButton:
+            state.$isHidden.withLock { $0 = false }
+            _ = state.routes.popLast()
+            
+        case let .tappedSpot(spotId):
+            state.routes.append(.spotDetail(SpotDetailFeature.State(viewState: .other, spotId: spotId)))
+        }
+        
+        return .none
+    }
+    
+    
 }
 
 extension ArchiveCoordinator.State {
