be0c87c092471adbca7139973e0624ab8eea4062
loveyMelon <manciepmc@gmail.com>
2025-08-25 21:19:01 +0900

    fix: 누락된 에러뷰 재대응

--- Patata/Presentation/ErrorView/NetworkErrorView.swift
+++ Patata/Presentation/ErrorView/NetworkErrorView.swift
@@ -6,13 +6,57 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct NetworkErrorView: View {
+    
+    @Perception.Bindable var store: StoreOf<NetworkErrorFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        contentView
     }
 }
 
-#Preview {
-    NetworkErrorView()
+extension NetworkErrorView {
+    private var contentView: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Image("NoNetwork")
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+                .frame(width: 48, height: 48)
+            
+            VStack(alignment: .center, spacing: 4) {
+                Text("네트워크 연결이")
+                Text("원활하지 않습니다.")
+            }
+            .textStyle(.headlineS)
+            .foregroundStyle(.textDefault)
+            .padding(.top, 36)
+            
+            Text("네트워크 상태를 확인 후 다시 시도해주세요.")
+                .textStyle(.bodyM)
+                .foregroundStyle(.textInfo)
+                .padding(.top, 16)
+            
+            HStack {
+                Spacer()
+                
+                Text("스팟 둘러보러 가기")
+                    .textStyle(.subtitleM)
+                    .foregroundStyle(.white)
+                    .padding(.vertical, 14)
+                
+                Spacer()
+            }
+            .background(.black)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+            .padding(.bottom, 20)
+            .padding(.top, 36)
+            .padding(.horizontal, 75)
+            .asButton {
+                store.send(.viewEvent(.tappedButton))
+            }
+        }
+    }
 }
+
