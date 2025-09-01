8cdb09ddeba0d5346fd11e816ee85ce54716ab99
loveyMelon <manciepmc@gmail.com>
2025-09-01 23:17:26 +0900

    refactor: TabCase 구조 정리 및 메타 매핑 추가

---
+++ Patata/Presentation/Coordinator/Tab/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  Patata
+//
+//  Created by 김진수 on 9/1/25.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}
--- Patata/Resource/Enum/TabCase.swift
+++ Patata/Resource/Enum/TabCase.swift
@@ -13,3 +13,28 @@ enum TabCase {
     case archive
     case myPage
 }
+
+struct TabAsset {
+    let inactive: String
+    let active: String
+    let title: String
+}
+
+extension TabCase {
+    static let order: [TabCase] = [.home, .map, .archive, .myPage]
+    var asset: TabAsset {
+        switch self {
+        case .home:
+            return .init(inactive: "HomeInActive", active: "HomeActive", title: "홈")
+            
+        case .map:
+            return .init(inactive: "SpotInActive", active: "SpotActive", title: "내주변")
+            
+        case .archive:
+            return .init(inactive: "ArchiveInActiveTap", active: "ArchiveActiveTap", title: "아카이브")
+            
+        case .myPage:
+            return .init(inactive: "MyPageInActive", active: "MyPageActive", title: "My")
+        }
+    }
+}
