4e05f7ea3023179fa308d5a0d7a57e756bc34758
loveyMelon <manciepmc@gmail.com>
2025-12-09 17:07:39 +0900

    feat: Firebase Analytics 초기 설정

--- Patata/Presentation/PatataApp.swift
+++ Patata/Presentation/PatataApp.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 import GoogleSignIn
 import ComposableArchitecture
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil)  -> Bool {
+      FirebaseApp.configure()
+    
+      return true
+    }
+}
 
 @main
 struct PatataApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.scenePhase) private var scenePhase
     
     var body: some Scene {
